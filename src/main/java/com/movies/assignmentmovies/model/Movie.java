d0d61062871228156d5e0fd7a5b6989d932f8c33
Leon Listo <Leon.listo@hotmail.com>
2022-02-28 13:04:11 +0100

    CHORE: add entity for movie model

--- src/main/java/com/movies/assignmentmovies/model/Movie.java
+++ src/main/java/com/movies/assignmentmovies/model/Movie.java
@@ -1,13 +1,14 @@
 package com.movies.assignmentmovies.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "movies")
-
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String genre;
     private Integer releaseYear;
@@ -23,6 +24,10 @@ public class Movie {
         this.director = director;
         this.url = url;
         this.trailer = trailer;
+    }
+
+    public Movie() {
+        super();
     }
 
     public Long getId() {
