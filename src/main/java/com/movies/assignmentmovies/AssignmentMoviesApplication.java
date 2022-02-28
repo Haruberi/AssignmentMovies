c5b2cdc10652599e48b61f628bc24751396b7408
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-28 08:51:38 +0100

    CHORE: add project

---
+++ src/main/java/com/movies/assignmentmovies/AssignmentMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.movies.assignmentmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentMoviesApplication.class, args);
+    }
+
+}
