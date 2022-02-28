2c8cd90dbf4a4934737f7f614e2054fe034f7a8a
Leon Listo <Leon.listo@hotmail.com>
2022-02-28 13:07:27 +0100

    CHORE: update controller for MovieController

--- src/main/java/com/movies/assignmentmovies/controller/MovieController.java
+++ src/main/java/com/movies/assignmentmovies/controller/MovieController.java
@@ -1,4 +1,14 @@
 package com.movies.assignmentmovies.controller;
 
+import com.movies.assignmentmovies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
 }
