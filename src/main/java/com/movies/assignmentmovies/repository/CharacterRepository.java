dc3db74382b5946e7c0e3f06dd52c420028d941e
Leon Listo <Leon.listo@hotmail.com>
2022-02-28 14:54:03 +0100

    CHORE: add character and movie jparepository

--- src/main/java/com/movies/assignmentmovies/repository/CharacterRepository.java
+++ src/main/java/com/movies/assignmentmovies/repository/CharacterRepository.java
@@ -1,7 +1,9 @@
 package com.movies.assignmentmovies.repository;
 
+import com.movies.assignmentmovies.model.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacterRepository {
+public interface CharacterRepository extends JpaRepository<Character, Long> {
 }
--- src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
+++ src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
@@ -1,7 +1,10 @@
 package com.movies.assignmentmovies.repository;
 
+import com.movies.assignmentmovies.model.Franchise;
+import com.movies.assignmentmovies.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FranchiseRepository {
+public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 }
