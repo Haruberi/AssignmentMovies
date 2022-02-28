256348da094a5d53de02788f149f70bab2743cb4
Leon Listo <Leon.listo@hotmail.com>
2022-02-28 09:46:17 +0100

    CHORE: added database configs

--- src/main/java/com/movies/assignmentmovies/model/Movie.java
+++ src/main/java/com/movies/assignmentmovies/model/Movie.java
@@ -1,4 +1,83 @@
 package com.movies.assignmentmovies.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+
 public class Movie {
+    private Long id;
+    private String title;
+    private String genre;
+    private Integer releaseYear;
+    private String director;
+    private String url;
+    private String trailer;
+
+    public Movie(Long id, String title, String genre, Integer releaseYear, String director, String url, String trailer) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.director = director;
+        this.url = url;
+        this.trailer = trailer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
 }
