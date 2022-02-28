1d193461fc44155653b358dbb27332da12ab076d
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-28 10:14:31 +0100

    CHORE: add annotations for CharacterController

--- src/main/java/com/movies/assignmentmovies/controller/CharacterController.java
+++ src/main/java/com/movies/assignmentmovies/controller/CharacterController.java
@@ -1,4 +1,18 @@
 package com.movies.assignmentmovies.controller;
 
+import com.movies.assignmentmovies.model.Character;
+import com.movies.assignmentmovies.repository.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/character")
 public class CharacterController {
+
+    @Autowired
+    private CharacterRepository characterRepository;
+
+
 }
