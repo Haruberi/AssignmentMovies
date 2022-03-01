097cbe8ba4f360479c4a350abc8c0b8f587a7cf2
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-01 18:01:54 +0100

    CHORE: update @GeneratedValue

--- src/main/java/com/movies/assignmentmovies/model/Franchise.java
+++ src/main/java/com/movies/assignmentmovies/model/Franchise.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class Franchise {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long franchiseId;
     @Column(name="name")
         private String name;
