4ea929ea57d1260b7ae559d4493ee9a115437647
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-01 18:01:35 +0100

    CHORE: add franchise for one movie

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+INSERT INTO franchise ( description, name ) VALUES ( 'When a killer shark unleashes chaos on a beach community off Long Island, it is up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.', 'The Jaws Franchise' );
 INSERT INTO character ( alias, full_name, gender, url) VALUES ( 'Captain', 'Captain Quint', 'male', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FCAPTAIN.QUINT.JAWS%2F&psig=AOvVaw0JljCbS4OQJZtxFObrLX-5&ust=1646217400354000&source=images&cd=vfe&ved=2ahUKEwjNzZ7s26T2AhVYHMAKHZ37Bi4QjRx6BAgAEAk');
 INSERT INTO movie (director, genre, release_year, title, trailer, picture) VALUES
                                                       ('Steven Spielberg', 'Thriller',
