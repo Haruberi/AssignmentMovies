bc3cd2e7e866d27e58460fdc2a8000bd278d972b
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-01 09:23:22 +0100

    FEAT: add add autowired and get-, post- and put-mapping

--- src/main/java/com/movies/assignmentmovies/controller/CharacterController.java
+++ src/main/java/com/movies/assignmentmovies/controller/CharacterController.java
@@ -3,16 +3,45 @@ package com.movies.assignmentmovies.controller;
 import com.movies.assignmentmovies.model.Character;
 import com.movies.assignmentmovies.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/character")
+@RequestMapping(value="api/v1/character")
 public class CharacterController {
 
     @Autowired
     private CharacterRepository characterRepository;
 
+    @GetMapping()
+    public ResponseEntity<List<Character>> getAllCharacters() {
+        List<Character> characters = characterRepository.findAll();
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(characters,status);
+    }
 
+    @PostMapping
+    public ResponseEntity<Character> addCharacter(@RequestBody Character character) {
+        HttpStatus status;
+        character = characterRepository.save(character);
+        status = HttpStatus.CREATED;
+        return new ResponseEntity<>(character, status);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Character> updateCharacter(@PathVariable Long id, @RequestBody Character character) {
+        Character returnCharacter = new Character();
+        HttpStatus status;
+
+        if (!id.equals(character.getId())) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(returnCharacter, status);
+        }
+        returnCharacter = characterRepository.save(character);
+        status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(returnCharacter, status);
+    }
 }
