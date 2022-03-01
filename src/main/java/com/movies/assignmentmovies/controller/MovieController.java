30574a44e50463ef3750ed40925102370188726d
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-01 10:49:30 +0100

    REFACTOR: rename RequestMapping FEAT: add GetMapping

--- src/main/java/com/movies/assignmentmovies/controller/MovieController.java
+++ src/main/java/com/movies/assignmentmovies/controller/MovieController.java
@@ -3,16 +3,27 @@ package com.movies.assignmentmovies.controller;
 import com.movies.assignmentmovies.model.Movie;
 import com.movies.assignmentmovies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    @GetMapping()
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(movies, status);
+    }
+
 
 }
