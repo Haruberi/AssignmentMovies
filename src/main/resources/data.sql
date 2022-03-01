78aa04e4c8f544bb9846464b7c62a30234e4ea58
Leon Listo <Leon.listo@hotmail.com>
2022-03-01 12:11:34 +0100

    CHORE: implement new sql query

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO movie (director, genre, release_year, title, trailer, picture)VALUES
+INSERT INTO character ( alias, full_name, gender, url) VALUES ( 'Captain', 'Captain Quint', 'male', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FCAPTAIN.QUINT.JAWS%2F&psig=AOvVaw0JljCbS4OQJZtxFObrLX-5&ust=1646217400354000&source=images&cd=vfe&ved=2ahUKEwjNzZ7s26T2AhVYHMAKHZ37Bi4QjRx6BAgAEAk');
+INSERT INTO movie (director, genre, release_year, title, trailer, picture, character_id) VALUES
                                                       ('Steven Spielberg', 'Thriller',
                                                        1975, 'Jaws', 'https://www.youtube.com/watch?v=U1fu_sA7XhE',
-                                                       'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSvjq3-vt5bD0zu0uiek5P9wVywgas7CIi8v77BIUTobwAx0KvP');
+                                                       'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSvjq3-vt5bD0zu0uiek5P9wVywgas7CIi8v77BIUTobwAx0KvP',1);
