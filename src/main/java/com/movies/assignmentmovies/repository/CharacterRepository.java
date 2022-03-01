8c845dc82261be9242fdd8abbfa9d1c35f1d944a
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-01 09:24:06 +0100

    FEAT: add JpaRepository

--- src/main/java/com/movies/assignmentmovies/repository/CharacterRepository.java
+++ src/main/java/com/movies/assignmentmovies/repository/CharacterRepository.java
@@ -1,9 +1,10 @@
 package com.movies.assignmentmovies.repository;
 
-import com.movies.assignmentmovies.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.movies.assignmentmovies.model.Character;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+
 }
--- src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
+++ src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
@@ -1,7 +1,6 @@
 package com.movies.assignmentmovies.repository;
 
 import com.movies.assignmentmovies.model.Franchise;
-import com.movies.assignmentmovies.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
