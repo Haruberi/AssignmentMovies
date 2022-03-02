e2c675d30614e8dd151dbc9b9cef351841ac9228
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-02 11:29:13 +0100

    REFACTOR: add test comment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
 INSERT INTO franchise ( description, name ) VALUES ( 'When a killer shark unleashes chaos on a beach community off Long Island, it is up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.', 'The Jaws Franchise' );
 INSERT INTO character ( alias, full_name, gender, url) VALUES ( 'Captain', 'Captain Quint', 'male', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FCAPTAIN.QUINT.JAWS%2F&psig=AOvVaw0JljCbS4OQJZtxFObrLX-5&ust=1646217400354000&source=images&cd=vfe&ved=2ahUKEwjNzZ7s26T2AhVYHMAKHZ37Bi4QjRx6BAgAEAk');
+INSERT INTO character (alias, full_name, gender, url) VALUES ('Mr Test', 'Test Character', 'Male', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF1z7sRjfbQB1JkfMNB0jMFt1YhZj_vhhC9A&usqp=CAU');
 INSERT INTO movie (director, genre, release_year, title, trailer, picture) VALUES
                                                       ('Steven Spielberg', 'Thriller',
                                                        1975, 'Jaws', 'https://www.youtube.com/watch?v=U1fu_sA7XhE',
                                                        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSvjq3-vt5bD0zu0uiek5P9wVywgas7CIi8v77BIUTobwAx0KvP');
+
+
+--test--
