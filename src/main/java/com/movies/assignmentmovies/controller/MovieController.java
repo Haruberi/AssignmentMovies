e0492573019d52075f2a05b2b9c10e4d426251a6
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-03 08:59:50 +0100

    FEAT: add helpmethods into MovieController

--- src/main/java/com/movies/assignmentmovies/controller/MovieController.java
+++ src/main/java/com/movies/assignmentmovies/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.movies.assignmentmovies.controller;
 
 import com.movies.assignmentmovies.model.Character;
 import com.movies.assignmentmovies.model.Movie;
+import com.movies.assignmentmovies.repository.CharacterRepository;
 import com.movies.assignmentmovies.repository.MovieRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +49,11 @@ public class MovieController {
         return new ResponseEntity<>(returnMovie,status);
     }
 
+    /**
+     * Create a new movie.
+     * @param movie
+     * @return
+     */
     @PostMapping
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie){
         HttpStatus status;
@@ -56,6 +62,12 @@ public class MovieController {
         return new ResponseEntity<>(movie, status);
     }
 
+    /**
+     * Update an existing movie.
+     * @param id
+     * @param movie
+     * @return
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie){
         Movie returnMovie = new Movie();
@@ -69,24 +81,27 @@ public class MovieController {
         status = HttpStatus.NO_CONTENT;
         return new ResponseEntity<>(returnMovie, status);
     }
+
+    /**
+     * Delete a movie.
+     * @param id
+     * @return
+     */
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteCharacter(@PathVariable("id") long id) {
+    public ResponseEntity<HttpStatus> deleteMovie(@PathVariable("id") long id) {
         movieRepository.deleteById(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     /**
-     * Delete all characters.
+     * Delete all movies.
      * @return
      */
     @DeleteMapping()
-    public ResponseEntity<HttpStatus> deleteAllCharacters() {
+    public ResponseEntity<HttpStatus> deleteAllMovies() {
         movieRepository.deleteAll();
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
-
 }
