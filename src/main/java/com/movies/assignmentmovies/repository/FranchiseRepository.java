a7a484fa6d825ce1786f3c3c4f818b2f7db7c8e8
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-03-04 13:37:26 +0100

    CHORE: implement SQL query to get all characters in FranchiseRepository

--- src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
+++ src/main/java/com/movies/assignmentmovies/repository/FranchiseRepository.java
@@ -1,9 +1,19 @@
 package com.movies.assignmentmovies.repository;
 
+import com.movies.assignmentmovies.model.Character;
 import com.movies.assignmentmovies.model.Franchise;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
+
+
+    @Query(value = "SELECT cha FROM character_franchise AS c JOIN AS cha ON cha.character_id = c.character_id WHERE c.franchise_id = ?1", nativeQuery = true)
+    List<Character> getAllCharactersInFranchise(long character_id);
 }
